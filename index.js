cd0ca38f3357761a2c380caf6ab6ec9294fa5321
Nafiul Alam <nafiulalam30@gmail.com>
2022-04-29 21:06:34 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zlsf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("db connected");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get("/", (req, res) => {
+    res.send("server running");
+})
+
+app.listen(port, () => {
+    console.log("listening to port ", port);
+})
