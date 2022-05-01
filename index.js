75ecd027d172152d4a4aefa4905b0a5e6293ba43
Nafiul Alam <nafiulalam30@gmail.com>
2022-05-01 20:32:59 +0600

    products get api working

--- index.js
+++ index.js
@@ -10,12 +10,32 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1zlsf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log("db connected");
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("stockpile");
+        const productCollection = database.collection("products");
+        const emailCollection = database.collection('emails');
+
+        app.get("/products", async (req, res) => {
+            const page = parseInt(req.query?.page) || 0;
+            const size = parseInt(req.query.size);
+            const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            if (page || size) {
+                products = await cursor.skip(size * page).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+            res.send(products);
+        });
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
